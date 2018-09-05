28100ed844120f2a14e0086e5f378929e2a4e3e5
kennest <kennyoulai@gmail.com>
2018-09-05 23:19:54 +0000

    Details is OK 5/09/2018 19:30

--- app/src/main/java/com/labs/botdev/zouglou/activities/DetailsEventActivity.java
+++ app/src/main/java/com/labs/botdev/zouglou/activities/DetailsEventActivity.java
@@ -5,6 +5,7 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -23,8 +24,29 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.jean.jcplayer.JcPlayerManagerListener;
+import com.example.jean.jcplayer.general.JcStatus;
 import com.example.jean.jcplayer.model.JcAudio;
 import com.example.jean.jcplayer.view.JcPlayerView;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.labs.botdev.zouglou.R;
 import com.labs.botdev.zouglou.objectbox.Address;
 import com.labs.botdev.zouglou.objectbox.Address_;
@@ -34,9 +56,10 @@ import com.labs.botdev.zouglou.objectbox.Event;
 import com.labs.botdev.zouglou.objectbox.Event_;
 import com.labs.botdev.zouglou.objectbox.Place;
 import com.labs.botdev.zouglou.objectbox.Place_;
-import com.labs.botdev.zouglou.tasks.Player;
 import com.labs.botdev.zouglou.utils.AppController;
 import com.labs.botdev.zouglou.utils.Constants;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -46,21 +69,18 @@ import java.util.List;
 import de.hdodenhof.circleimageview.CircleImageView;
 import io.objectbox.Box;
 
-public class DetailsEventActivity extends AppCompatActivity {
+public class DetailsEventActivity extends AppCompatActivity implements Player.EventListener {
     ImageView event_picture;
     Toolbar toolbar;
-    TextView description,date;
-    FloatingActionButton navigation_top,navigation_bottom;
-    Box<Place> placeBox=AppController.boxStore.boxFor(Place.class);
-    Box<Address> addressBox=AppController.boxStore.boxFor(Address.class);
-    Box<Artist> artistBox=AppController.boxStore.boxFor(Artist.class);
-     boolean playPause;
-     MediaPlayer mediaPlayer;
-     boolean initialStage = true;
-     List<Artist> artists=new ArrayList<>();
-     LinearLayout artists_layout;
-     JcPlayerView jcplayerView;
-
+    TextView description, date;
+    FloatingActionButton navigation_top, navigation_bottom;
+    Box<Place> placeBox = AppController.boxStore.boxFor(Place.class);
+    Box<Address> addressBox = AppController.boxStore.boxFor(Address.class);
+    Box<Artist> artistBox = AppController.boxStore.boxFor(Artist.class);
+    List<Artist> artists = new ArrayList<>();
+    LinearLayout artists_layout;
+    PlayerView playerView;
+    SimpleExoPlayer player;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -68,15 +88,12 @@ public class DetailsEventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
         event_picture = findViewById(R.id.header);
         toolbar = findViewById(R.id.anim_toolbar);
-        description=findViewById(R.id.event_description);
-        date=findViewById(R.id.event_date);
-        navigation_top=findViewById(R.id.navigation_top);
-        navigation_bottom=findViewById(R.id.navigation_bottom);
-        artists_layout=findViewById(R.id.artists_layout);
-        jcplayerView = (JcPlayerView) findViewById(R.id.jcplayer);
-
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        //description = findViewById(R.id.event_description);
+        //date = findViewById(R.id.event_date);
+        navigation_top = findViewById(R.id.navigation_top);
+        navigation_bottom = findViewById(R.id.navigation_bottom);
+        artists_layout = findViewById(R.id.artists_layout);
+        playerView=findViewById(R.id.playerView);
 
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         setSupportActionBar(toolbar);
@@ -92,28 +109,26 @@ public class DetailsEventActivity extends AppCompatActivity {
                 .load(Constants.UPLOAD_URL + e.getPicture())
                 .into(event_picture);
 
-        Place p=placeBox.query().equal(Place_.raw_id,e.getPlace_id()).build().findFirst();
-        Address a=addressBox.query().equal(Address_.place_id,p.getRaw_id()).build().findFirst();
+        Place p = placeBox.query().equal(Place_.raw_id, e.getPlace_id()).build().findFirst();
+        Address a = addressBox.query().equal(Address_.place_id, p.getRaw_id()).build().findFirst();
 
 
-
-        collapsingToolbarLayout.setTitle(e.getTitle()+"/"+p.getTitle());
+        collapsingToolbarLayout.setTitle(e.getTitle() + "/" + p.getTitle());
 
         toolbar.setTitle(e.getBegin());
-        description.setText(e.getDescription());
-        date.setText(String.format("%s /%s", e.getBegin(), e.getEnd()));
+        //date.setText(String.format("%s /%s", e.getBegin(), e.getEnd()));
 
         navigation_top.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ShowOnMap(a.getLatitude(),a.getLongitude());
+                ShowOnMap(a.getLatitude(), a.getLongitude());
             }
         });
 
         navigation_bottom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ShowOnMap(a.getLatitude(),a.getLongitude());
+                ShowOnMap(a.getLatitude(), a.getLongitude());
             }
         });
         extractRecipient(e.getArtists_id());
@@ -152,19 +167,34 @@ public class DetailsEventActivity extends AppCompatActivity {
         }
     }
 
+    protected void playSample(String url, ImageView play, ImageView pause) {
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        DefaultTrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
 
-    protected void playSample(String url,ImageView play,ImageView pause){
-        ArrayList<JcAudio> jcAudios = new ArrayList<>();
-        jcAudios.add(JcAudio.createFromURL(Constants.UPLOAD_URL+url));
-        jcplayerView.initPlaylist(jcAudios, null);
-        jcplayerView.setVisibility(View.VISIBLE);
+// 2. Create the player
+        SimpleExoPlayer  player = ExoPlayerFactory.newSimpleInstance(getApplicationContext(), trackSelector);
+
+        playerView.setPlayer(player);
+        playerView.setVisibility(View.VISIBLE);
+
+// Produces DataSource instances through which media data is loaded.
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(getApplicationContext(), Util.getUserAgent(getApplicationContext(), "yourApplicationName"));
+// This is the MediaSource representing the media to be played.
+        MediaSource audioSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse(url));
+// Prepare the player with the source.
+        player.prepare(audioSource);
+        player.addListener(this);
+
+        player.setPlayWhenReady(true);
     }
 
-    private void loadArtists(List<Artist> artists){
-        for(Artist a:artists) {
-            Toast.makeText(getApplicationContext(),"Artist added"+a.getName(),Toast.LENGTH_LONG).show();
-            LinearLayout parent= new LinearLayout(getApplicationContext());
-            parent= (LinearLayout) getLayoutInflater().inflate(R.layout.artist_item, null);
+    private void loadArtists(List<Artist> artists) {
+        for (Artist a : artists) {
+            Toast.makeText(getApplicationContext(), "Artist added" + a.getName(), Toast.LENGTH_LONG).show();
+            LinearLayout parent = new LinearLayout(getApplicationContext());
+            parent = (LinearLayout) getLayoutInflater().inflate(R.layout.artist_item, null);
             CircleImageView avatar = parent.findViewById(R.id.avatar);
             ImageView pause = parent.findViewById(R.id.pause);
             ImageView play = parent.findViewById(R.id.play);
@@ -177,7 +207,7 @@ public class DetailsEventActivity extends AppCompatActivity {
             parent.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    playSample(Constants.UPLOAD_URL+a.getSample(),play,pause);
+                    playSample(Constants.UPLOAD_URL + a.getSample(), play, pause);
                 }
             });
             parent.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
@@ -185,20 +215,20 @@ public class DetailsEventActivity extends AppCompatActivity {
         }
     }
 
-    private void extractRecipient(String s){
-        Toast.makeText(getApplicationContext(),"ID: "+s,Toast.LENGTH_LONG).show();
-        Box<Artist> recipientBox=AppController.boxStore.boxFor(Artist.class);
-        String replace = s.replace("[","");
+    private void extractRecipient(String s) {
+        Toast.makeText(getApplicationContext(), "ID: " + s, Toast.LENGTH_LONG).show();
+        Box<Artist> recipientBox = AppController.boxStore.boxFor(Artist.class);
+        String replace = s.replace("[", "");
         System.out.println(replace);
-        String replace1 = replace.replace("]","");
+        String replace1 = replace.replace("]", "");
         System.out.println(replace1);
 
         List<String> IDlists = new ArrayList<String>(Arrays.asList(replace1.split(",")));
 
-        for(String n:IDlists){
-            int i=Integer.parseInt(n.trim());
-            Artist x=artistBox.query().equal(Artist_.raw_id,i).build().findFirst();
-            if(x!=null)
+        for (String n : IDlists) {
+            int i = Integer.parseInt(n.trim());
+            Artist x = artistBox.query().equal(Artist_.raw_id, i).build().findFirst();
+            if (x != null)
                 artists.add(x);
         }
     }
@@ -206,17 +236,97 @@ public class DetailsEventActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        if (mediaPlayer != null) {
-            mediaPlayer.reset();
-            mediaPlayer.release();
-            mediaPlayer = null;
-        }
     }
 
     @Override
     public boolean onNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    private void InitExoPlayer() {
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        DefaultTrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+// 2. Create the player
+        SimpleExoPlayer  init_player = ExoPlayerFactory.newSimpleInstance(getApplicationContext(), trackSelector);
+        player=init_player;
+        playerView.setPlayer(player);
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, @Nullable Object manifest, int reason) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+        switch (playbackState) {
+
+            case Player.STATE_BUFFERING:
+
+                break;
+
+            case Player.STATE_ENDED:
+                playerView.setVisibility(View.INVISIBLE);
+                break;
+
+            case Player.STATE_IDLE:
+
+                break;
+
+            case Player.STATE_READY:
+
+                break;
+
+            default:
+
+                break;
+
+        }
+
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+    @Override
+    public void onSeekProcessed() {
+
     }
 }
 
